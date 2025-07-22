975d3a2507505e30e2312899f3d688437360c39c
Pratikshyarout <pratikshyarout98@gmail.com>
2025-07-22 23:13:37 +0530

    my commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+// backend/db.js
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+console.log("📦 Connecting to DB:", process.env.DB_NAME);
+
+module.exports = pool;
